bf02614264cdadc2840a774b686099491f313987
Gidro <Gidroshvandel@gmail.com>
2017-05-26 09:45:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gidro/myapplication/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.gidro.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    private void clickLogin(View view) {
+
+
+
+    }
+    private void clickRegistration(View view) {
+
+
+
+    }
+
+    private void showPassword(View view) {
+
+
+
+    }
+
+    private void hidePassword(View view) {
+
+
+
+    }
+}
